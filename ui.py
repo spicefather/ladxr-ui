8f5a456aa3c8e73af0ecb526fb486bbb0cbd6ece
spicefather <25182947+spicefather@users.noreply.github.com>
2020-06-18 17:33:22 -0400

    Updated for witch randomization

--- ui.py
+++ ui.py
@@ -28,6 +28,7 @@ class LadxrUi(tk.Frame):
       { 'text': 'Randomize Seashells', 'arg': 'seashells', 'type': 'boolean', 'default': True },
       { 'text': 'Keysanity', 'arg': 'keysanity', 'type': 'boolean', 'default': False },
       { 'text': 'Randomize Heart Containers', 'arg': 'heartcontainers', 'type': 'boolean', 'default': False },
+      { 'text': 'Randomize Witch', 'arg': 'witch', 'type': 'boolean', 'default': True },
       { 'text': 'Add items to owl statues', 'arg': 'owlstatues', 'type': 'choice', 'choices': [
         { 'text': 'Never', 'value': 'none' },
         { 'text': 'Dungeons', 'value': 'dungeon' },
